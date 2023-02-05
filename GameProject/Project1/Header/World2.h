a4fc980c25b13391b3cddd3d0feef75b5654c9dd
ZyChua2000 <122003260+ZyChua2000@users.noreply.github.com>
2023-02-05 15:48:13 +0800

    Cleaned up world state

---
+++ GameProject/Project1/Header/World2.h
@@ -0,0 +1,77 @@
+/******************************************************************************/
+/*!
+\file		World2.h
+\author 	Chua Zheng Yang
+\par    	email: c.zhengyang\@digipen.edu
+\date   	February 02, 2023
+\brief		This header file contains the functions for the level of World.
+
+Copyright (C) 2023 DigiPen Institute of Technology.
+Reproduction or disclosure of this file or its contents without the
+prior written consent of DigiPen Institute of Technology is prohibited.
+ */
+ /******************************************************************************/
+
+
+
+// ---------------------------------------------------------------------------
+
+/******************************************************************************/
+/*!
+	"Load" function of this state
+	This function loads all necessary assets for the World level.
+	It should be called once before the start of the level.
+	It loads assets like textures, meshes and music files etcÅc
+*/
+/******************************************************************************/
+void GS_World2_Load(void);
+
+/******************************************************************************/
+/*!
+	"Initialize" function of this state
+	This function initialises all the values of the World state. It should
+	be called once at the start of the level.
+*/
+/******************************************************************************/
+void GS_World2_Init(void);
+
+
+/******************************************************************************/
+/*!
+	"Update" function of this state
+	This function updates the game logic, physics and collision. It runs while
+	the game loop runs for the World state.
+*/
+/******************************************************************************/
+void GS_World2_Update(void);
+
+/******************************************************************************/
+/*!
+	"Draw" function of this state
+	This function draws all the shapes onto the game screen, called repeatedly
+	during game loop.
+*/
+/******************************************************************************/
+void GS_World2_Draw(void);
+
+/******************************************************************************/
+/*!
+	"Free" function of this state
+	This function frees all the instances created for the World level.
+*/
+/******************************************************************************/
+void GS_World2_Free(void);
+
+/******************************************************************************/
+/*!
+	"Unload" function of this state
+	This function frees all the shapes and assets that were loaded for the
+	World level.
+*/
+/******************************************************************************/
+void GS_World2_Unload(void);
+
+// ---------------------------------------------------------------------------
+
+
+
