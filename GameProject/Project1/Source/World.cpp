c8a66da6bd8aa7d4dd0a921b91aa98c6bc130e66
javierlow98 <113163737+javierlow98@users.noreply.github.com>
2023-02-17 22:19:11 +0800

    code fix

--- GameProject/Project1/Source/World.cpp
+++ GameProject/Project1/Source/World.cpp
@@ -16,6 +16,7 @@ prior written consent of DigiPen Institute of Technology is prohibited.
 #include <fstream>
 #include <iostream>
 
+
 /*!
 	Defines
 */
@@ -199,8 +200,8 @@ void GS_World_Load(void) {
 	RefLine->refMesh = true;
 
 	//BUGGY CODE, IF UANBLE TO LOAD, CANNOT USE DEBUGGING MODE
-	s8 font = AEGfxCreateFont("Assets/OpenSans-Regular.ttf", 12);
-	FontList[FontListNum++] = font;
+	//s8 font = AEGfxCreateFont("Assets/OpenSans-Regular.ttf", 12);
+	//FontList[FontListNum++] = font;
 }
 
 /******************************************************************************/
