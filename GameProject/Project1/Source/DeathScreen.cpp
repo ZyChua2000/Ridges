cf2201f6e27327071237916fe0424e5b59be5a79
Chengrong <chengrong98.cr@gmail.com>
2023-03-30 14:48:57 +0800

    Revert "Merge branch 'main' into chengrong"
    This reverts commit f367c7d602dc74da1c870ebb727e873a2310e95c, reversing
    changes made to 0d65c35d6ec62bb915c9666fbc0f0620933eca21.

--- GameProject/Project1/Source/DeathScreen.cpp
+++ GameProject/Project1/Source/DeathScreen.cpp
@@ -155,11 +155,7 @@ void GS_DeathScreen_Update(void) {
 	if (AEInputCheckTriggered(AEVK_4)) {
 		gGameStateNext = GS_COLOSSEUM;
 	}
-	if (AEInputCheckTriggered(AEVK_H)) {
-		
-		gGameStateNext = GS_HELP;
-		return;
-	}
+
 	s32 mX, mY;
 	AEInputGetCursorPosition(&mX, &mY);
 	mouseX = float(mX);
