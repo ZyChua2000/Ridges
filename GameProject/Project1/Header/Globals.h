125fb45bdb814546725db102eb267fe9532148f8
ZyChua2000 <122003260+ZyChua2000@users.noreply.github.com>
2023-03-31 18:00:07 +0800

    Cleaned up globals

--- GameProject/Project1/Header/Globals.h
+++ GameProject/Project1/Header/Globals.h
@@ -2,11 +2,17 @@
 #ifndef GLOBALS
 #define GLOBALS
 
+/******************************************************************************/
+/*!
+	Objects-related Globals for all levels
+*/
+/******************************************************************************/
+
 static const unsigned int	GAME_OBJ_NUM_MAX = 32;				// The total number of unique objects (Shapes)
 static const unsigned int	TEXTURE_NUM_MAX = 32;				// The total number of Textures
-static const unsigned int	GAME_OBJ_INST_NUM_MAX = 2048;			// The total number of dynamic game object instances
+static const unsigned int	GAME_OBJ_INST_NUM_MAX = 124;			// The total number of dynamic game object instances
 static const unsigned int	FONT_NUM_MAX = 10;					// The total number of fonts
-static const unsigned int	STATIC_OBJ_INST_NUM_MAX = 1024;		// The total number of static game object instances
+static const unsigned int	STATIC_OBJ_INST_NUM_MAX = 124;		// The total number of static game object instances
 
 // list of original object
 extern GameObj				sGameObjList[GAME_OBJ_NUM_MAX];				// Each element in this array represents a unique game object (shape)
@@ -23,50 +29,26 @@ extern unsigned long		sStaticObjInstNum;							// The number of used static game
 extern s8					FontList[FONT_NUM_MAX];						// Each element in this array represents a Font
 extern unsigned long		FontListNum;								// The number of used fonts
 
-extern bool					levelCleared[4];
-
 extern const unsigned long FLAG_ACTIVE;			// For whether object instance is active
 
+/******************************************************************************/
+/*!
+	Cell related globals
+*/
+/******************************************************************************/
 
 extern const int			SPRITE_SCALE;			// Scale of sprites
 extern const int			TEXTURE_MAXWIDTH;		// Canvas Width of sprite sheet
 extern const int			TEXTURE_MAXHEIGHT;		// Canvas Height of sprite sheet
 extern const float			TEXTURE_CELLSIZE;		// Number of pixel size for each sprite
-
-extern const int			COLLISION_LEFT;			// 0001
-extern const int			COLLISION_RIGHT;		// 0010
-extern const int			COLLISION_TOP;			// 0100
-extern const int			COLLISION_BOTTOM;		// 1000
-
-extern const float			TOWER_RIGHT;
-extern const float			TOWER_LEFT;
-extern const float			TOWER_UP;
-extern const float			TOWER_DOWN;
-
 extern const int			CAM_CELL_WIDTH;			// Number of cells in viewport for width
 extern const int			CAM_CELL_HEIGHT;		// Number of cells in viewport for height
 
-extern const float			BOUNDING_RECT_SIZE;		// this is the normalized bounding rectangle (width and height) sizes - AABB collision data
-extern const float			PLAYER_SPEED;			// Player movement sped
-extern const float			NPC_SPEED;				// Mob movement speed
-extern const float			BULLET_SPEED;
-
-extern const float			SLASH_COOLDOWN_t;		// Time for slash to cooldown
-extern const float			WALK_COOLDOWN_t;		// Time for walk to be stopped after slash
-extern const float			DAMAGE_COODLDOWN_t;		// Time for damage invulenrability to reset
-extern const float			SPIKE_TRANSITION_t;		// Time for spike to fade in/out
-extern const float			SPIKE_DEATH_t;			// Duration for spike to be non-existent
-extern const float			SPIKE_LIFE_t;			// Duration for spike to be existent
-extern const int			MAX_PLAYER_HEALTH;
-extern const float			slashRange;
-extern const float			enemySightRange;
-extern const float			pickUpRange;
-
-extern const float			TOWER_REFRESH;			// Rate at which tower shoot bullets
-
-extern const float			aoeREFRESH;
-extern const float			challengeATKREFRESH;
-
+/******************************************************************************/
+/*!
+	Texture related globals
+*/
+/******************************************************************************/
 extern const AEVec2			TEXTURE_POTION;
 extern const AEVec2			TEXTURE_TOWER;
 extern const AEVec2			TEXTURE_BULLET;
@@ -85,34 +67,88 @@ extern const AEVec2			TEXTURE_FLOOR;
 extern const AEVec2			TEXTURE_FENCE;
 extern const AEVec2			TEXTURE_BOSSCIRCLE;
 
+/******************************************************************************/
+/*!
+	Collision related globals
+*/
+/******************************************************************************/
 
-extern bool damageFlag;
-extern AEAudio HeroDamaged;
-extern AEAudioGroup Damage;
-extern AEAudio HeroSlash;
-extern AEAudio BulletShot;
-extern AEAudioGroup BulletGroup;
+extern const int			COLLISION_LEFT;			// 0001
+extern const int			COLLISION_RIGHT;		// 0010
+extern const int			COLLISION_TOP;			// 0100
+extern const int			COLLISION_BOTTOM;		// 1000
+extern const float			BOUNDING_RECT_SIZE;		// this is the normalized bounding rectangle (width and height) sizes - AABB collision data
 
-extern AEAudio Interact;
-extern AEAudioGroup InteractGroup;
+/******************************************************************************/
+/*!
+	Logic related globals
+*/
+/******************************************************************************/
 
-extern AEAudio Movement;
-extern AEAudioGroup MovementGroup;
+//Enemy globals
+extern const float			NPC_SPEED;				// Mob movement speed
+extern const float			enemySightRange;		// Distance from which mobs will pathfind towards player
 
+//Tower Globals
+extern const float			TOWER_REFRESH;			// Rate at which tower shoot bullets
+extern const float			BULLET_SPEED;			// Bullet movement speed
+extern const float			TOWER_RIGHT;			// Direction of tower facing right
+extern const float			TOWER_LEFT;				// Direction of tower facing left
+extern const float			TOWER_UP;				// Direction of tower facing up
+extern const float			TOWER_DOWN;				// Direction of tower facing down
 
+//Boss Globals
+extern const float			aoeREFRESH;				// Time for AOE attack to be refreshed
+extern const float			challengeATKREFRESH;	// Time for Challenges attack to be refreshed
 
+//Spike globals
+extern const float			SPIKE_TRANSITION_t;		// Time for spike to fade in/out
+extern const float			SPIKE_DEATH_t;			// Duration for spike to be non-existent
+extern const float			SPIKE_LIFE_t;			// Duration for spike to be existent
 
+//Player globals
+extern const float			PLAYER_SPEED;			// Player movement speed
+extern const int			MAX_PLAYER_HEALTH;		// Max player health
+extern const float			pickUpRange;			// Range for player to pick up items
+extern const float			slashRange;				// Range of distance for slash to hit enemy
+extern const float			SLASH_COOLDOWN_t;		// Time for slash to cooldown
+extern const float			WALK_COOLDOWN_t;		// Time for walk to be stopped after slash
+extern const float			DAMAGE_COODLDOWN_t;		// Time for damage invulenrability to reset
+
+/******************************************************************************/
+/*!
+	Audio related globals
+*/
+/******************************************************************************/
+
+extern AEAudio				HeroDamaged;
+extern AEAudio				HeroSlash;
+extern AEAudio				BulletShot;
+extern AEAudio				Interact;
+extern AEAudio				Movement;
+extern AEAudioGroup			InteractGroup;
+extern AEAudioGroup			BulletGroup;
+extern AEAudioGroup			MovementGroup;
+extern AEAudioGroup			Damage;
+
+/******************************************************************************/
+/*!
+	Other globals
+*/
+/******************************************************************************/
+
+extern bool					damageFlag;
 static float				mouseX;					// Position of mouse X
 static float				mouseY;					// Position of mouse Y
-
 static f32					camX;					// Position of camera X
 static f32					camY;					// Position of camera Y
+
+extern bool					levelCleared[3];		// Array of boolean checking which level is cleared
 
 enum levelCleared  {
 	tower = 0,
 	maze,
 	colosseum,
-	blackhole
 };
 
 #endif
